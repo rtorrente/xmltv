f01bb58869c3d0d955a6c0439f79d0b8c68fa9ca
rtorrente <romain@smartfire.pro>
2021-12-13 10:40:32 +0100

    Updates

--- scripts/ga_publish_git.sh
+++ scripts/ga_publish_git.sh
@@ -3,8 +3,8 @@
 now=$(date +"%d/%m/%Y %H:%M:%S-%Z")
 
 force_push () {
-    git config --global user.name 'XMLTV GithubAction'
-    git config --global user.email 'XMLTV@users.noreply.github.com'
+    git config --global user.name 'GithubAction'
+    git config --global user.email 'GithubAction@users.noreply.github.com'
     git add --all
     LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
     NEW_COMMIT_MESSAGE="[ACTION]update TV guides ($now)"
