468e5d0c25f0fa04255c8ab9c01f5213d71c7936
GithubAction <GithubAction@users.noreply.github.com>
2021-12-20 07:46:15 +0000

    [ACTION] update TV guides (02/07/2026 06:31:08-UTC)

---
+++ scripts/ga_publish_git.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+now=$(date +"%d/%m/%Y %H:%M:%S-%Z")
+
+force_push () {
+    git config --global user.name 'GithubAction'
+    git config --global user.email 'GithubAction@users.noreply.github.com'
+    git add --all
+    LAST_COMMIT_MESSAGE=$(git log -1 --pretty=%B)
+    NEW_COMMIT_MESSAGE="[ACTION] update TV guides ($now)"
+    if [[ $LAST_COMMIT_MESSAGE =~ ^"[ACTION]".* ]] ;
+    then
+      echo -e "\n- Last COMMIT is auto commit, we amend the commit\n"
+      git commit --amend -m "$NEW_COMMIT_MESSAGE"
+    else
+      echo -e "\n- Last COMMIT is other commit, we create a new commit\n"
+      git commit -m "$NEW_COMMIT_MESSAGE"
+    fi
+    git push -f origin
+}
+
+echo -e "\n- Force push changes\n"
+force_push
+
+echo -e "\n- Changes have been pushed --> exit\n"
+exit 0
